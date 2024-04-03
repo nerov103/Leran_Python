511919b315e394b76eb8260cb7370cbbb8f2a655
nerov103 <nibir01000@gmail.com>
2024-04-04 02:29:48 +0600

    something new added and some chnage in this script

---
+++ WsCube Tech/Multiplication_Table.py
@@ -0,0 +1,4 @@
+#now to day ima create muliplication table
+table = int(input("enter your number here:"))
+for num in range(1, 11):
+    print(table, "x", num, "=", table*num)
---
+++ WsCube Tech/celsuis_to_fahrenheit.py
@@ -0,0 +1,3 @@
+celsius = int(input("enter temperaturn in celsius:"))
+fahrenheit = celsius*(1.8)+32 #how to (1.5) and 9/5 aita toy ta taka ja kono akta use kroa hoba
+print("the converted value is",fahrenheit,"Fahrenheit")
