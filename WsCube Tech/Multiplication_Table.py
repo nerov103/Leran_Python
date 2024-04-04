3ad28cb24f448e0f9f6b7823cfab704658dad13e
nerov103 <nibir01000@gmail.com>
2024-04-05 01:01:45 +0600

    something chnage in this code today

--- WsCube Tech/Multiplication_Table.py
+++ WsCube Tech/Multiplication_Table.py
@@ -1,4 +1,9 @@
 #now to day ima create muliplication table
 table = int(input("enter your number here:"))
-for num in range(1, 11):
-    print(table, "x", num, "=", table*num)
+
+#use for loop
+# for num in range(1, 11):
+    # print(table, "x", num, "=", table*num)
+
+#use to while loop
+i = 10
